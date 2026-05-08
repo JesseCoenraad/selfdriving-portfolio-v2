802c2fefc8194c69b1e84c26bca23d43d6cdcd4d
Jesse Coenraad <jessecoenraad2004@gmail.com>
2026-05-08 12:51:09 +0200

    LAUNCH

--- launchers/default.sh
+++ launchers/default.sh
@@ -8,13 +8,7 @@ dt-launchfile-init
 # YOUR CODE BELOW THIS LINE
 # ----------------------------------------------------------------------------
 
-
-# NOTE: Use the variable DT_REPO_PATH to know the absolute path to your code
-# NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
-
-# launching app
-dt-exec echo "This is an empty launch script. Update it to launch your application."
-
+dt-exec roslaunch --wait encoder_pose encoder_pose_node.launch veh:=$VEHICLE_NAME
 
 # ----------------------------------------------------------------------------
 # YOUR CODE ABOVE THIS LINE
